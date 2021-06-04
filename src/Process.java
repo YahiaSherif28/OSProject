58f4cc9ee9ad0a7ea07d3609488cd5d1c8a64bc8
YahiaSherif28 <yahiaabdelbar@gmail.com>
2021-06-04 02:07:59 +0200

    Add assign

--- src/Process.java
+++ src/Process.java
@@ -17,15 +17,29 @@ public class Process {
     }
 
     public void run() throws Exception {
-        for(String instruction : processInstructions) {
+        for (String instruction : processInstructions) {
             String[] instructionParts = instruction.split(" ");
-            if(instructionParts[0].equals("print")) {
+            if (instructionParts[0].equals("print")) {
                 String output = getVariableOrString(instructionParts[1]);
                 print(output);
-            } else {
-                // rest here
+            } else if (instructionParts[0].equals("assign")) {
+                String variableName = instructionParts[1];
+                String source = instructionParts[2];
+                String fileName = source.equals("readFile") ? instructionParts[3] : null;
+                assign(variableName, source, fileName);
             }
         }
+    }
+
+    private void assign(String variableName, String source, String fileName) throws Exception {
+        if (source.equals("input")) {
+            processVariables.put(variableName, input());
+        } else if (source.equals("readFile")) {
+            processVariables.put(variableName, readFile(fileName));
+        }
+    }
+
+    private String readFile(String fileName) {
     }
 
     public String getVariableOrString(String varName) {
